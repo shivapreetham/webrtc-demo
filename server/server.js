4229e7473a07d3db4faa45089ed859b6d10c14cf
shivapreetham <2005shivapreetham@gmail.com>
2025-03-17 20:42:47 +0530

    server port changes

--- server/server.js
+++ server/server.js
@@ -1,42 +1,47 @@
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+
+// Bind to the PORT environment variable; default to 10000 if not provided
+const PORT = process.env.PORT || 10000;
+const wss = new WebSocket.Server({ port: PORT });
+
+console.log(`WebSocket server running on port ${PORT}`);
 
 let rooms = {};
 
 wss.on("connection", (ws) => {
-    ws.on("message", (message) => {
-        const data = JSON.parse(message);
-        const { type, room } = data;
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+    const { type, room } = data;
 
-        switch (type) {
-            case "join":
-                if (!rooms[room]) {
-                    rooms[room] = [];
-                }
-                rooms[room].push(ws);
-                console.log(`User joined room: ${room}`);
-                break;
-
-            case "offer":
-            case "answer":
-            case "candidate":
-                if (rooms[room]) {
-                    rooms[room].forEach((client) => {
-                        if (client !== ws && client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify(data));
-                        }
-                    });
-                }
-                break;
+    switch (type) {
+      case "join":
+        if (!rooms[room]) {
+          rooms[room] = [];
         }
-    });
+        rooms[room].push(ws);
+        console.log(`User joined room: ${room}`);
+        break;
 
-    ws.on("close", () => {
-        Object.keys(rooms).forEach((room) => {
-            rooms[room] = rooms[room].filter((client) => client !== ws);
-            if (rooms[room].length === 0) {
-                delete rooms[room];
+      case "offer":
+      case "answer":
+      case "candidate":
+        if (rooms[room]) {
+          rooms[room].forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify(data));
             }
-        });
+          });
+        }
+        break;
+    }
+  });
+
+  ws.on("close", () => {
+    Object.keys(rooms).forEach((room) => {
+      rooms[room] = rooms[room].filter((client) => client !== ws);
+      if (rooms[room].length === 0) {
+        delete rooms[room];
+      }
     });
+  });
 });
