8f6afa0bc0a18e30e619cc9aee8434e6d8aff167
shivapreetham <2005shivapreetham@gmail.com>
2025-08-09 21:59:25 +0530

    port chnaged to 10000

--- server/server.js
+++ server/server.js
@@ -236,5 +236,5 @@ setInterval(() => {
   }
 }, 5 * 60 * 1000);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
